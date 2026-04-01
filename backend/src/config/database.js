25e860af62b875afac3c2ea60ccb0e41e587e9d2
davidgallardotapia <davidgallardotapia@gmail.com>
2026-04-01 15:05:46 -0300

    fix database config for render

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,13 +2,18 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: isProduction ? { rejectUnauthorized: false } : false,
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      ssl: false,
+    });
 
 module.exports = pool;
